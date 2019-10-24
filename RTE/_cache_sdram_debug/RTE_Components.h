33cf3828c8365f56cf8cbd163db340ee0bb311fe
Markus <mail@algorithmen.de>
2019-10-24 22:31:53 +0200

    Initial Version

---
+++ RTE/_cache_sdram_debug/RTE_Components.h
@@ -0,0 +1,27 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'cache' 
+ * Target:  'cache sdram_debug' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "fsl_device_registers.h"
+
+/*  NXP::Device:SDK Drivers:xip_board:2.0.0 */
+#define XIP_EXTERNAL_FLASH 1
+#define XIP_BOOT_HEADER_ENABLE 1
+/*  NXP::Device:SDK Drivers:xip_device:2.0.0 */
+#define XIP_EXTERNAL_FLASH 1
+#define XIP_BOOT_HEADER_ENABLE 1
+
+
+#endif /* RTE_COMPONENTS_H */
